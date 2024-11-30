d31ecfa15047c4d1a8d0fe1698bf9601420a79a1
WoahAI <115117306+Woahai321@users.noreply.github.com>
2024-11-30 17:51:28 -0500

    🏁 ✅ Final v0.3.3.1
    changed default port to 8777 instead of 8000

--- seerrbridge.py
+++ seerrbridge.py
@@ -1231,4 +1231,4 @@ async def on_close():
 # Main entry point for running the FastAPI server
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8777)
